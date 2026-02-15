27a908e3527f840e656584f343b085da9895bc24
yonatanpro12r-maker <yonatanpro12r@gmail.com>
2026-02-15 08:26:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
 
